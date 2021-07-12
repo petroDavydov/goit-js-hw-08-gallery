839b988c3a209232e4959f1729e222a4f29f0044
petroDavydov <petrodavydov@gmail.com>
2021-07-12 18:43:53 +0300

    ok2

--- appSecond.js
+++ appSecond.js
@@ -9,7 +9,6 @@ const refs = {
   overlayEl: document.querySelector(".lightbox__overlay"),
 };
 
-
 refs.mainGallery.addEventListener("click", isGalleryImage);
 refs.btnLightBox.addEventListener("click", closeLightBoxWindow);
 refs.modal.addEventListener("click", closeLightBoxImage);
@@ -44,58 +43,52 @@ const cardsItem = createImgCardMarkup(galleryItems); // вивел всею ра
 refs.mainGallery.insertAdjacentHTML("beforeend", cardsItem); // вставил разметку
 // console.log(mainGalery);
 
-
 function isGalleryImage(event) {
-	  const isGalleryImageElement = event.target.classList.contains(
-	    "gallery__image"
-	  );
-	  if (!isGalleryImageElement) {
-	    return;
-	  } else if (isGalleryImageElement) {
-	    event.preventDefault();
-	    refs.lightBoxEl.classList.add("is-open"); // добавил клас на елемент
-	    refs.lightboxImage.src = event.target.getAttribute("data-source"); // взял из шаблонних строк для  замени картинки на большую
-	    refs.lightboxImage.alt = event.target.alt; // добавил описание через alt
-	    // console.log(lightboxImage); // виводит адрес и альт
-	  }
-	  window.addEventListener("keyup", keyEscape); // добавил прослушиватель на кнопку `Escape`
-	}
-	
+  const isGalleryImageElement = event.target.classList.contains(
+    "gallery__image"
+  );
+  if (!isGalleryImageElement) {
+    return;
+  } else if (isGalleryImageElement) {
+    event.preventDefault();
+    refs.lightBoxEl.classList.add("is-open"); // добавил клас на елемент
+    refs.lightboxImage.src = event.target.getAttribute("data-source"); // взял из шаблонних строк для  замени картинки на большую
+    refs.lightboxImage.alt = event.target.alt; // добавил описание через alt
+    // console.log(lightboxImage); // виводит адрес и альт
+  }
+  window.addEventListener("keyup", keyEscape); // добавил прослушиватель на кнопку `Escape`
+}
 
-	function closeLightBoxWindow() {		
-		refs.lightBoxEl.classList.remove("is-open");
-		refs.lightboxImage.src = "";
-		refs.lightboxImage.alt = "";
-		window.removeEventListener("keyup", keyEscape);
-	  }
-	  
+function closeLightBoxWindow() {
+  refs.lightBoxEl.classList.remove("is-open");
+  refs.lightboxImage.src = "";
+  refs.lightboxImage.alt = "";
+  window.removeEventListener("keyup", keyEscape);
+}
 
-	  function closeLightBoxImage(event) {
-		if (event.target === event.currentTarget) {
-		  closeLightBoxWindow();
+function closeLightBoxImage(event) {
+  if (event.target === event.currentTarget) {
+    closeLightBoxWindow();
+  }
+}
 
-		}
-	  }
+function keyEscape(event) {
+  if (event.code === "Escape") {
+    closeLightBoxWindow();
+  }
+}
 
+function onOverlayClose() {
+  // создал функцию закрития по клику на оверлей
+  closeLightBoxWindow();
+}
 
-	  function keyEscape(event) {
-		if (event.code === "Escape") {
-		  closeLightBoxWindow();
-		}
-	  }
+//   ===============
 
-	  function onOverlayClose() {
-		  // создал функцию закрития по клику на оверлей
-		  closeLightBoxWindow();
-		}
-		
-	//   ===============
-
-	function arrow(event) {
-		if (event.code == 37) {
-		  // дайте хотяби намек
-		} else if (event.code == 39) {
-		  // что сюда писать
-		}
-	  }
-	  
+function arrow(event) {
+  if (event.code == 37) {
+    // дайте хотяби намек
+  } else if (event.code == 39) {
+    // что сюда писать
+  }
+}
